970514dce8fdd82680e4f3d0566f2899c4f06a26
dhtpa <dhtpa@39.123.63.147>
2024-04-29 11:40:35 +0900

    commit 9
    게시판검색기능구현

--- board/src/main/java/com/board/domain/Page.java
+++ board/src/main/java/com/board/domain/Page.java
@@ -73,30 +73,67 @@ public class Page {
 	}
 
 	public boolean getNext() {
-		 return next;
+		return next;
 	}
 
-
 	private void dataCalc() {
-		 
-		 // 마지막 번호
-		 endPageNum = (int)(Math.ceil((double)num / (double)pageNumCnt) * pageNumCnt);
-		 
-		 // 시작 번호
-		 startPageNum = endPageNum - (pageNumCnt - 1);
-		 
-		 // 마지막 번호 재계산
-		 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNumCnt));
-		 
-		 if(endPageNum > endPageNum_tmp) {
-		  endPageNum = endPageNum_tmp;
-		 }
-		 
-		 prev = startPageNum == 1 ? false : true;
-		 next = endPageNum * pageNumCnt >= count ? false : true;
-		 
-		 displayPost = (num - 1) * postNum;
-		 
+
+		// 마지막 번호
+		endPageNum = (int) (Math.ceil((double) num / (double) pageNumCnt) * pageNumCnt);
+
+		// 시작 번호
+		startPageNum = endPageNum - (pageNumCnt - 1);
+
+		// 마지막 번호 재계산
+		int endPageNum_tmp = (int) (Math.ceil((double) count / (double) pageNumCnt));
+
+		if (endPageNum > endPageNum_tmp) {
+			endPageNum = endPageNum_tmp;
 		}
+
+		prev = startPageNum == 1 ? false : true;
+		next = endPageNum * pageNumCnt >= count ? false : true;
+
+		displayPost = (num - 1) * postNum;
+
+	}
+	// 검색어타입과 검색어
+	/*
+	 * private String searchTypeKeyword;
+	 * 
+	 * public void setSearchTypeKeyword(String searchType, String keyword) {
+	 * 
+	 * if (searchType.equals("") || keyword.equals("")) { searchTypeKeyword = ""; }
+	 * else { searchTypeKeyword = "&searchType=" + searchType + "&keyword=" +
+	 * keyword; } }
+	 */
+
+	public String getSearchTypeKeyword() {
+
+		if (searchType.equals("") || keyword.equals("")) {
+			return "";
+		} else {
+			return "&searchType=" + searchType + "&keyword=" + keyword;
+		}
+	}
+
+	private String searchType;
+	private String keyword;
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
 
 }
